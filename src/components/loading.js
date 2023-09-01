26921418942e119366d2c5e4b52aa2f842c50f84
363daisy <363daisy@gmail.com>
2023-09-01 19:45:25 +0900

    [FEAT]로딩페이지 구현

--- src/components/loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Loading = () => {
-  return(
-    <img src="/hommage-home.svg" alt="hommage_home" className='scale-105 -left-8 mb-32'/>    
-  )
-};
-
-export default Loading;
