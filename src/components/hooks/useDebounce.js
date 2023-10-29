e3d9f73c08e8252dae645d7e89eebbbdf402fd79
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2023-10-29 23:39:43 +0900

    [FIX] 모바일헤더 스크롤 방향에 반응

---
+++ src/components/hooks/useDebounce.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+export default function useDebounce(func, wait) {
+  const [id, setId] = useState(null)
+  useEffect(() => {
+    return () => {
+      clearTimeout(id)
+    }
+  }, [id])
+  return (...args) => {
+    if (id) {
+      clearTimeout(id)
+    }
+    setId(
+      setTimeout(() => {
+        setId(null)
+        func(...args)
+      }, wait)
+    )
+  }
+}
